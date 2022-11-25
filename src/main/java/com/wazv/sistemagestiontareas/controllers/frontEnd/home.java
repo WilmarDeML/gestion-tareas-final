6ca677597bfaba216e63500c22312b05f5f0ab5e
Wilmar Zapata <wavilla@stefanini.com>
2022-11-24 22:54:40 -0500

    Creando home

--- src/main/java/com/wazv/sistemagestiontareas/controllers/frontEnd/home.java
+++ src/main/java/com/wazv/sistemagestiontareas/controllers/frontEnd/home.java
@@ -1,6 +1,5 @@
 package com.wazv.sistemagestiontareas.controllers.frontEnd;
 
-import com.wazv.sistemagestiontareas.models.entities.Task;
 import com.wazv.sistemagestiontareas.models.entities.User;
 import com.wazv.sistemagestiontareas.services.TaskService;
 import com.wazv.sistemagestiontareas.services.UserService;
@@ -10,11 +9,10 @@ import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Optional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
-public class Index {
+public class home {
 
     @Autowired
     private TaskService taskService;
@@ -22,24 +20,16 @@ public class Index {
     @Autowired
     UserService userService;
 
-    @GetMapping
-    public String index(Model model, @AuthenticationPrincipal OidcUser principal) {
-        String auth0 = "";
+    @GetMapping("home/{idUsuario}")
+    public String index(Model model, @PathVariable String idUsuario) {
         try {
-            if (principal != null) {
-                auth0 = principal.getClaimAsString("sub").substring(6);
-                User user = userService.getByAuth0(auth0);
-                if ( user == null) {
-                    user = userService.createUser(principal.getClaims());
-                }
-                model.addAttribute("idUsuario", auth0);
-                model.addAttribute("name", user.getEmail());
-                model.addAttribute("urlImagen", user.getImage());
-            }
+            User usuario = userService.getByAuth0(idUsuario);
+            model.addAttribute("name", usuario.getEmail());
+            model.addAttribute("idUsuario", idUsuario);
+            model.addAttribute("urlImagen", usuario.getImage());
             model.addAttribute("title", "Sistema-gestión-tareas");
             return "index";
         } catch (Exception e) {
-            model.addAttribute("idUsuario", auth0);
             model.addAttribute("mensaje", e.getMessage() != null ? e.getMessage() : "Intente más tarde Ocurrió un error ".concat(e.getLocalizedMessage()));
             return "error";
         }
